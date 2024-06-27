43ba1cb4cc57645ccf99558f21604d626e136c69
Michaela Vojtechovska <mich.vojtechovska@gmail.com>
2024-06-27 20:58:37 +0200

    :wrench: Prep for testing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,8 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
+<script lang="ts">
+    import { createETInput } from "$lib";
+	
+</script>
+
+<h1>Test your eye-tracking device with the develex-core library</h1>
+<p>Use the form below to choose the type of eye-tracker you want to test in a browser environment. Remember, for remote eye-trackers, you need to have the device connected to your computer, and running the corresponding software running and the develex-bridge.</p>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
