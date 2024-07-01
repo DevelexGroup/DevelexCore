5a7210b5a7195e04bbfe9270111a29ddd7138ccd
Michaela Vojtechovska <mich.vojtechovska@gmail.com>
2024-07-01 13:27:05 +0200

    :sparkles: Control: Add SceneInputStateLog component for displaying input state logs

---
+++ src/component/SceneInputStateLog.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import type { GazeInputMessage } from "$lib/GazeInput/GazeInputEvent";
+    import { gazeInputStore } from "../store/gazeInputStore";
+
+    let inputStateMessageLogs: GazeInputMessage[] = [];
+
+    const addMessage = (message: GazeInputMessage) => {
+        inputStateMessageLogs = [...inputStateMessageLogs, message];
+    };
+
+    $: if ($gazeInputStore) {
+        $gazeInputStore.on("message", addMessage);
+    }
+
+</script>
+
+
+<ul>
+    {#each inputStateMessageLogs as message, i}
+        <li>
+            <span>{new Date(message.timestamp).toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit', second: '2-digit' })}</span>
+            <span>{message.type}</span>
+            <span>{message.value}</span>
+        </li>
+    {/each}
+</ul>
+
+<style>
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        border: 1px solid #dee2e6;
+        height: 500px;
+        overflow: scroll;
+    }
+    li {
+        display: grid;
+        grid-template-columns: 1fr 1fr 3fr;
+        text-align: left;
+        padding: 5px;
+        border-bottom: 1px solid #ced4da;
+    }
+</style>
