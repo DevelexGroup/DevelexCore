98b805fb73a1efd89fc8f826a963121f99a3b6ce
Michaela Vojtechovska <mich.vojtechovska@gmail.com>
2024-07-13 15:39:20 +0200

    :sparkles: Allow addInteraction logic for better typing and saving in Dexie db

--- src/store/sceneStores.ts
+++ src/store/sceneStores.ts
@@ -4,16 +4,38 @@ import type { GazeInputMessage } from '$lib/GazeInput/GazeInputEvent';
 import type { GazeInteractionObjectDwellEvent } from '$lib/GazeInteraction/GazeInteractionObject/GazeInteractionObjectDwellEvent';
 import type { GazeInteractionObjectSetFixationEvent } from '$lib/GazeInteraction/GazeInteractionObject/GazeInteractionObjectSet/GazeInteractionObjectSetFixationEvent';
 import type { GazeInteractionObjectSetSaccadeEvent } from '$lib/GazeInteraction/GazeInteractionObject/GazeInteractionObjectSet/GazeInteractionObjectSetSaccadeEvent';
+import type { Dwell } from '../database/models/Dwell';
+import dwellRepository from '../database/repositories/dwell.repository';
+import type { Fixation } from '../database/models/Fixation';
+import fixationRepository from '../database/repositories/fixation.repository';
+import type { Saccade } from '../database/models/Saccade';
+import saccadeRepository from '../database/repositories/saccade.repository';
 
 export const scenePointDataStore = writable<GazeDataCircularBuffer>(new GazeDataCircularBuffer(300));
 
 export const sceneStateStore = writable<GazeInputMessage[]>([]);
 
 // Define the writable store with a custom update function
-export const sceneObjectDwellStore = writable<GazeInteractionObjectDwellEvent[]>([]);
+export const sceneObjectDwellStore = writable<Dwell[]>([]);
 
 // Function to add new events to the store
-export const addDwellEvent = (event: GazeInteractionObjectDwellEvent) => {
+export const addDwellEvent = (unprocessedEvent: GazeInteractionObjectDwellEvent) => {
+    // Extract the relevant information from the event
+    const { type, sessionId, timestamp, duration, gazeData, target } = unprocessedEvent;
+    // convert target id to string
+    const aoi = target?.id.toString();
+
+    const event: Dwell = {
+        sessionId,
+        timestamp,
+        aoi,
+        duration,
+        gazeData,
+        type
+    };
+
+    void dwellRepository.create(event);
+
     sceneObjectDwellStore.update(events => {
         // Add the new event
         const updatedEvents = [event, ...events];
@@ -27,9 +49,26 @@ export const addDwellEvent = (event: GazeInteractionObjectDwellEvent) => {
     });
 }
 
-export const sceneObjectFixationStore = writable<GazeInteractionObjectSetFixationEvent[]>([]);
+export const sceneObjectFixationStore = writable<Fixation[]>([]);
 
-export const addFixationEvent = (event: GazeInteractionObjectSetFixationEvent) => {
+export const addFixationEvent = (unprocessedEvent: GazeInteractionObjectSetFixationEvent) => {
+    // Extract the relevant information from the event
+    const { type, sessionId, timestamp, duration, gazeData, target } = unprocessedEvent;
+    // convert target, which is array of Elements, id to string delimited by ;
+    // check if Array.isArray(target) is true
+    const aoi = Array.isArray(target) ? target.map((t) => t.id.toString()).join(';') : '';
+
+    const event: Fixation = {
+        type,
+        sessionId,
+        timestamp,
+        aoi,
+        duration,
+        gazeData
+    };
+
+    void fixationRepository.create(event);
+
     sceneObjectFixationStore.update(events => {
         // Add the new event
         const updatedEvents = [event, ...events];
@@ -43,9 +82,31 @@ export const addFixationEvent = (event: GazeInteractionObjectSetFixationEvent) =
     });
 }
 
-export const sceneObjectSaccadeStore = writable<GazeInteractionObjectSetSaccadeEvent[]>([]);
+export const sceneObjectSaccadeStore = writable<Saccade[]>([]);
 
-export const addSaccadeEvent = (event: GazeInteractionObjectSetSaccadeEvent) => {
+export const addSaccadeEvent = (unprocessedEvent: GazeInteractionObjectSetSaccadeEvent) => {
+    // Extract the relevant information from the event
+    const { type, sessionId, timestamp, duration, distance, gazeData, target, originGazeData, angleToScreen, angleToPrevious, angleToPreviousInvalidityTime } = unprocessedEvent;
+    // convert target, which is array of Elements, id to string delimited by ;
+    // check if Array.isArray(target) is true
+    const aoi = Array.isArray(target) ? target.map((t) => t.id.toString()).join(';') : '';
+
+    const event: Saccade = {
+        type,
+        sessionId,
+        timestamp,
+        aoi,
+        duration,
+        distance,
+        gazeData,
+        originGazeData,
+        angleToScreen,
+        angleToPrevious,
+        angleToPreviousInvalidityTime
+    };
+
+    void saccadeRepository.create(event);
+
     sceneObjectSaccadeStore.update(events => {
         // Add the new event
         const updatedEvents = [event, ...events];
